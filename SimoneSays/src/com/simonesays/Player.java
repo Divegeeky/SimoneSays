211b3f055cbd13e6893caed5b434af3f2e1bd00b
Shane Thoney <shane.thoney@asu.edu>
2014-10-05 19:56:26 -0700

    Update Player.java

--- SimoneSays/src/com/simonesays/Player.java
+++ SimoneSays/src/com/simonesays/Player.java
@@ -1,5 +1,47 @@
 package com.simonesays;
 
 public class Player {
+	private String name = "";
+	private int score = 0;
+	private int currentLevel = 0;
+	private int[] inputArray;
+	
+	public Player() {
+		
+	}
+	
+	public Player(String name){
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getCurrentLevel() {
+		return currentLevel;
+	}
+
+	public void setCurrentLevel(int currentLevel) {
+		this.currentLevel = currentLevel;
+	}
+
+	public int[] getByteArray() {
+		return inputArray;
+	}
+
+	public void setInputArray(int[] inputArray) {
+		this.inputArray = inputArray;
+	}	
 }
