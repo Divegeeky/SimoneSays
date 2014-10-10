e61af8804f1ef51fb108e48c8681697821663413
Wyatt Draggoo <wyatt.draggoo@asu.edu>
2014-10-09 22:28:16 -0700

    Added incrementCurrentLevel to Player.java, and added something pseudo-code-ish to SimoneCore to perform a level.

--- SimoneSays/src/com/simonesays/Player.java
+++ SimoneSays/src/com/simonesays/Player.java
@@ -36,6 +36,10 @@ public class Player {
 	public void setCurrentLevel(int currentLevel) {
 		this.currentLevel = currentLevel;
 	}
+	
+	public void incrementCurrentLevel() {
+		this.currentLevel++;
+	}
 
 	public int[] getByteArray() {
 		return inputArray;
--- SimoneSays/src/com/simonesays/SimoneCore.java
+++ SimoneSays/src/com/simonesays/SimoneCore.java
@@ -20,7 +20,7 @@ public class SimoneCore {
         SimoneUI simone = new SimoneUI();
         displayHighScore();
                 
-        
+        Player currentplayer = new Player();
     }
 
     private List<Integer> currentPattern = new ArrayList<Integer>();
@@ -43,8 +43,8 @@ public class SimoneCore {
     // Should also have one of these that computes the score directly from the
     // current Player level
     public int computeScore(Player p) {
-    	 int n = p.getCurrentLevel();
-    	 return ((n * (n+1)) / 2);
+    	int n = p.getCurrentLevel();
+    	return ((n * (n+1)) / 2);
     }
 
     // Should also have one of these that computes the score directly from the
@@ -64,20 +64,53 @@ public class SimoneCore {
     public static void startGame() {
     
     }
+    
+    public void displayCurrentPattern() {
+    	for( Integer i: this.currentPattern ) {
+    		switch (i) {
+    			case 0: SimoneUI.bluebutton.blink();
+    				break;
+    			case 1: SimoneUI.greenbutton.blink();
+    				break;
+    			case 2: SimoneUI.redbutton.blink();
+    				break;
+    			case 3: SimoneUI.yellowbutton.blink();
+    				break;
+    		}
+    	}
+    }
+
+	public void doLevel() {
+		this.inputPattern.clear();
+		this.addToPattern();
+		this.displayCurrentPattern();
+		
+		for(Integer i: this.currentPattern) {
+			// Don't need the integer, but it will ask for the same number of inputs as are in the current pattern
+			this.inputPattern.add(this.getUserInput());
+		}
+		
+		if (this.checkPattern()) {
+			this.currentplayer.incrementCurrentLevel();
+			this.doLevel();
+		} else {
+			HighScore.processScore(this.calculateScore(currentplayer));
+		}
+	}
 
     public static void quitGame() {
     }
     //Added routine here to get the HS.  Not exactly sure how we are to display that in the UI.  Needs work still.
     public static void displayHighScore() throws ClassNotFoundException, IOException {
-        	Player[] hsList = HighScore.getHSList();
-        	//need to add how to display here.
-        	for (int i=0; i < hsList.length; i++){
+        Player[] hsList = HighScore.getHSList();
+        //need to add how to display here.
+        for (int i=0; i < hsList.length; i++){
         	SimoneUI.HighScoreUI.append(hsList[i].toString());
-        	}
         }
+    }
  
 
-    public void checkPattern() {
+    public boolean checkPattern() {
     }
 
     // Not sure if this should be here, or if we should always submit to the
